03b52fa13d281483c3cbb182733b85d82c61d8ae
Raimov Rinat <raimovrinat@yandex.ru>
2018-07-23 15:39:23 +0300

    updates

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var json;
+function searchCategory() {
+	getCategoryList();
+}
 
 function getCategoryList() {
 
@@ -10,43 +12,53 @@ function getCategoryList() {
   }
   else {
     json = xhr.responseText;
+    parseJSON(json);
   }
 }
 
-function searchCategory() {
+function parseJSON(json) {
 
   var data = JSON.parse(json);
   var rusNames = [];
+  var engNames = [];
   var ids = [];
 
   for ( var i = 0; i < data.length; i++) {
    rusNames.push(data[i].name);
+   engNames.push(data[i].englishName);
    ids.push(data[i].id);
   }
 
-	var searchValue = document.getElementById("search-field").value;
-	var list = document.getElementById("result-list");
-	var table = document.getElementById("result-table");
-	var rusNamesFinal = [];
-	var idsFinal = [];
-		for (var i = 0; i < rusNames.length; i++) {
-			if(rusNames[i].indexOf(searchValue.toUpperCase()) > -1) {
-				console.log(rusNames[i] + " " + ids[i]);
-				rusNamesFinal.push(rusNames[i]);
-				idsFinal.push(ids[i])
-			}
-		}
+  findCategory(rusNames,engNames,ids)
+}
 
-	var row , cell, text;
-	for (var i = 0; i < rusNamesFinal.length; i++) {
-		// if(rusNamesFinal[i].charAt(0) == searchValue.charAt(0)) {
-			row = table.insertRow(1);
-			cell1 = row.insertCell(0);
-			cell2 = row.insertCell(1);
-			text1 = document.createTextNode(rusNamesFinal[i]);
-			text2 = document.createTextNode(idsFinal[i]);
-				cell1.appendChild(text1);
-				cell2.appendChild(text2);
-			// }
-	}
+function findCategory(rusNames,engNames,ids) {
+  var searchValue = document.getElementById("search-field").value;
+  var rusNamesFinal = [];
+  var englishNamesFinal = [];
+  var idsFinal = [];
+
+  for (var i = 0; i < rusNames.length; i++) {
+    if(engNames[i].indexOf(searchValue.toUpperCase()) > -1) {
+      engNamesFinal.push(engNames[i]);
+      idsFinal.push(ids[i])
+    }
+  }
+
+  showCategory(rusNamesFinal,englishNamesFinal,idsFinal)
+}
+
+function showCategory(rusNamesFinal,engNamesFinal,idsFinal) {
+  var row, cell, text;
+  var table = document.getElementById("result-table");
+
+  for (var i = 0; i < engNamesFinal.length; i++) {
+    row = table.insertRow(1);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+    text1 = document.createTextNode(engNamesFinal[i]);
+    text2 = document.createTextNode(idsFinal[i]);
+    cell1.appendChild(text1);
+    cell2.appendChild(text2);
+  }
 }
