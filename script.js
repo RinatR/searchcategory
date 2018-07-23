852151c9997a40bf38f9301a5e1bf3f0a0cf915e
Raimov Rinat <raimovrinat@yandex.ru>
2018-07-23 13:56:24 +0300

    aa

--- script.js
+++ script.js
@@ -28,34 +28,35 @@
       } else {
         // вывести результат
         console.log( xhr.responseText ); // responseText -- текст ответа.
+      }
     }
 
-		function search() {
-			var searchValue = document.getElementById("search-field").value;
-			var list = document.getElementById("result-list");
-			var table = document.getElementById("result-table");
-			var rusNamesFinal = [];
-			var idsFinal = [];
-				for (var i = 0; i < rusNames.length; i++) {
-					if(rusNames[i].indexOf(searchValue.toUpperCase()) > -1) {
-						console.log(rusNames[i] + " " + ids[i]);
-						rusNamesFinal.push(rusNames[i]);
-						idsFinal.push(ids[i])
-					}
-				}
+		// function search() {
+		// 	var searchValue = document.getElementById("search-field").value;
+		// 	var list = document.getElementById("result-list");
+		// 	var table = document.getElementById("result-table");
+		// 	var rusNamesFinal = [];
+		// 	var idsFinal = [];
+		// 		for (var i = 0; i < rusNames.length; i++) {
+		// 			if(rusNames[i].indexOf(searchValue.toUpperCase()) > -1) {
+		// 				console.log(rusNames[i] + " " + ids[i]);
+		// 				rusNamesFinal.push(rusNames[i]);
+		// 				idsFinal.push(ids[i])
+		// 			}
+		// 		}
 
-				var row , cell, text;
-				for (var i = 0; i < rusNamesFinal.length; i++) {
-					// if(rusNamesFinal[i].charAt(0) == searchValue.charAt(0)) {
-						row = table.insertRow(1);
-						cell1 = row.insertCell(0);
-						cell2 = row.insertCell(1);
-						text1 = document.createTextNode(rusNamesFinal[i]);
-						text2 = document.createTextNode(idsFinal[i]);
-	  					cell1.appendChild(text1);
-	  					cell2.appendChild(text2);
-	  				// }
-				}
+		// 		var row , cell, text;
+		// 		for (var i = 0; i < rusNamesFinal.length; i++) {
+		// 			// if(rusNamesFinal[i].charAt(0) == searchValue.charAt(0)) {
+		// 				row = table.insertRow(1);
+		// 				cell1 = row.insertCell(0);
+		// 				cell2 = row.insertCell(1);
+		// 				text1 = document.createTextNode(rusNamesFinal[i]);
+		// 				text2 = document.createTextNode(idsFinal[i]);
+	 //  					cell1.appendChild(text1);
+	 //  					cell2.appendChild(text2);
+	 //  				// }
+		// 		}
 
 
-		}
+		// }
